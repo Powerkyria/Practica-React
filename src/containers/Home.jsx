f17ec252f93f1325e774e35a87ea4627742861c1
Powerkyria <gitteando@gmail.com>
2020-11-09 22:01:29 +0000

    change name App.jsx to Home.jsx

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -10,7 +10,7 @@ import '../assets/styles/app.scss';
 
 const API = 'http://localhost:3000/initalState';
 
-const App = () => {
+const Home = () => {
 	const InitialState = useInitialState(API);
 	return InitialState.length === 0 ? <h1>Loading... </h1> : ( 
 		<div className="App">
@@ -41,4 +41,4 @@ const App = () => {
 	);
 }
 
-export default App;
+export default Home;

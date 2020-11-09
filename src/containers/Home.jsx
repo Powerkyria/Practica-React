03a013adae34313ea39c328c3a60c7aa6afc0963
Powerkyria <gitteando@gmail.com>
2020-11-09 22:20:58 +0000

    delete header, footer and className='App'

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
-import Footer from '../components/Footer';
 import useInitialState from '../hooks/useInitialState'
 import '../assets/styles/app.scss';
 
@@ -13,8 +11,7 @@ const API = 'http://localhost:3000/initalState';
 const Home = () => {
 	const InitialState = useInitialState(API);
 	return InitialState.length === 0 ? <h1>Loading... </h1> : ( 
-		<div className="App">
-			<Header />
+		<>
 			<Search />
 			{InitialState.mylist.length > 0 && 
 				<Categories title="Mi lista">
@@ -36,8 +33,7 @@ const Home = () => {
 				{InitialState.originals.map((item) => <CarouselItem key={item.id} {...item} />)}
 				</Carousel>
 			</Categories>
-			<Footer />
-		</div>
+		</>
 	);
 }
 

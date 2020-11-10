7ca4728c5671802a424a8a9c06f893c50ee7fb26
Powerkyria <gitteando@gmail.com>
2020-11-10 17:53:18 +0000

    add input classname validation

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -9,7 +9,7 @@ const Search = ({ isHome }) => {
 	return (
 		<section className="main">
 			<h2 className="main__title">¿Qué quieres ver hoy?</h2>
-			<input type="text" className="input" placeholder="Buscar..." />
+			<input type="text" className={intputStyle} placeholder="Buscar..." />
 		</section>
 	);
 };
